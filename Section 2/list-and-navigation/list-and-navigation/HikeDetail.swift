3683c427d007049667b7aad159ab927ea802ca09
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-04-07 19:13:27 +0700

    Feature(DetailView): add tab and zooming animation

--- Section 2/list-and-navigation/list-and-navigation/HikeDetail.swift
+++ Section 2/list-and-navigation/list-and-navigation/HikeDetail.swift
@@ -10,15 +10,22 @@ import SwiftUI
 
 struct HikeDetail: View {
     let hike: Hike
+    @State private var zoomed: Bool = false
     
     var body: some View {
         VStack {
             Image(hike.imageUrl)
-            .resizable()
-                .aspectRatio(contentMode: .fit)
+                .resizable()
+                .aspectRatio(contentMode: self.zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        self.zoomed.toggle()
+                    }
+            }
+            
             Text(hike.name)
             Text(String(format: "%.2f", hike.miles))
-        }
+        }.navigationBarTitle(Text(hike.name), displayMode: .inline)
     }
 }
 
