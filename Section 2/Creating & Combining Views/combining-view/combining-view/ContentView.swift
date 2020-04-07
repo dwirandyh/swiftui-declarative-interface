858652db8553b84ecacee102ab2a55fbbc0a7b95
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-04-07 17:23:54 +0700

    Initialize section

---
+++ Section 2/Creating & Combining Views/combining-view/combining-view/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  combining-view
+//
+//  Created by Dwi Randy Herdinanto on 07/04/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("First Line")
+            Text("Second Line")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
