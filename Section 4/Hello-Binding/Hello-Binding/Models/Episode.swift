e7b7f8351bed7522f71fff88a1024f9475d02b10
Dwi Randy Herdinanto <dwi.randy@amarbank.co.id>
2020-04-08 16:32:56 +0700

    Feautre(Binding): Add example binding project

---
+++ Section 4/Hello-Binding/Hello-Binding/Models/Episode.swift
@@ -0,0 +1,14 @@
+//
+//  Episode.swift
+//  Hello-Binding
+//
+//  Created by Dwi Randy Herdinanto on 08/04/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import Foundation
+
+struct Episode {
+    let name: String
+    let track: String
+}
